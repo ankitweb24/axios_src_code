a5f988edcf35826abd61489dbf494715625b11c6
ankitweb24 <ankitjangir232323@gmail.com>
2023-08-30 06:22:06 +0530

    first commit

--- app/page.js
+++ app/page.js
@@ -1,95 +1,71 @@
-import Image from 'next/image'
-import styles from './page.module.css'
+"use client";
+import axios, {Axios} from 'axios'
+import { useEffect, useState } from 'react'
 
-export default function Home() {
+const page = () => {
+
+  const [data, setData] = useState([]);
+  const [error, setError] = useState("");
+  const [loading, setLoading] = useState(true)
+
+
+  const api = `https://dummyjson.com/products`
+
+  const getApidata =  async (url) =>{
+    try {
+      const result = await axios.get(url);
+      setData(result.data.products)
+      setLoading(false)
+    } catch (error) {
+      setError(error.message)
+      setLoading(false)
+    }
+  }
+
+
+useEffect(() => {
+  getApidata(api);
+}, [])
+
+  
+
+  // useEffect(() => {
+  //   axios.get(`https://dummyjson.com/products`)
+  //   .then((res) =>{
+  //     // console.log(res.data.products);
+  //     setData(res.data.products)
+  //   }).catch(err => {setError(err.message)})
+  // }, [])
+  
+  
+  
+
+  if(loading) {
+    return (
+      <h1>Loading ....</h1>
+    )
+  }
+
+
   return (
-    <main className={styles.main}>
-      <div className={styles.description}>
-        <p>
-          Get started by editing&nbsp;
-          <code className={styles.code}>app/page.js</code>
-        </p>
-        <div>
-          <a
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className={styles.vercelLogo}
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
+    <>
 
-      <div className={styles.center}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
+{error !== "" && <h2>{error}</h2> }
 
-      <div className={styles.grid}>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Docs <span>-&gt;</span>
-          </h2>
-          <p>Find in-depth information about Next.js features and API.</p>
-        </a>
+      {data.map((element, index) => {
+        const {title, thumbnail, price} = element;
 
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Learn <span>-&gt;</span>
-          </h2>
-          <p>Learn about Next.js in an interactive course with&nbsp;quizzes!</p>
-        </a>
+        return (
+          <div className='card' key={index}>
+            <h4>{title}</h4>
+            <img src={thumbnail} alt="" />
+            <small>{price}</small>
+          </div>
+        )
 
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Templates <span>-&gt;</span>
-          </h2>
-          <p>Explore the Next.js 13 playground.</p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className={styles.card}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2>
-            Deploy <span>-&gt;</span>
-          </h2>
-          <p>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
+      })}
+    </>
   )
 }
+
+export default page
